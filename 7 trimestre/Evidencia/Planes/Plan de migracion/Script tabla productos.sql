819fc830ada265a3e2525131bcfb4e60579fb9b8
Enyeer27 <130396566+Enyeer27@users.noreply.github.com>
2024-04-03 11:14:47 -0500

    trabajo

---
+++ 7 trimestre/Evidencia/Planes/Plan de migracion/Script tabla productos.sql
@@ -0,0 +1,12 @@
+INSERT INTO Productos (Id_Producto, Id_Dato_Personal, Id_Peso, Id_Categoria, Id_Reserva, Nombre_Producto, Cantidad, Descripcion, Url_Imagen)
+VALUES
+(1, 1, 'Unidad', 'Fruta', 1, 'Manzana', 7, 'Rica manzana roja', 'Manzana.png'),
+(2, 2, 'Unidad', 'Fruta', 2, 'Mango', 3, 'Mango dulce', 'Mango.jpg'),
+(3, 3, 'Libras', 'Verdura', 3, 'Tomate', 5, 'Fresco', 'Tomate.jpg'),
+(4, 4, 'Libras', 'Fruta', 4, 'Pera', 2, 'Pera para consumo', 'Pera.jpg'),
+(5, 5, 'Kilos', 'Verdura', 5, 'Cebolla', 1, 'Cebolla cabezona', 'Cebolla.png'),
+(6, 6, 'Libras', 'Verdura', 6, 'Pimenton', 9, '2 pimentones maduros', 'Pimenton.jpg'),
+(7, 7, 'Libras', 'Fruta', 7, 'Durazno', 4, 'Bolsitas de 20 duraznos', 'Durazno.png'),
+(8, 8, 'Kilos', 'Fruta', 8, 'Melon', 2, 'Para jugo', 'Melon.png'),
+(9, 9, 'Unidad', 'Fruta', 9, 'Sandia', 3, 'Unidad', 'Sandia.png'),
+(10, 10, 'Libras', 'Fruta', 10, 'Piña', 3, 'Dulcecita la piña', 'Piña.jpg');
